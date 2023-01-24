793880eb19456e7cd0ea80adaaea8cce21096f20
leticias20 <leticia.sanchez83@gmail.com>
2023-01-24 12:33:59 -0600

    Initial commit

---
+++ src/main/java/com/betterjavacode/springbatchdemo/BetterjavacodeApplication.java
@@ -0,0 +1,13 @@
+package com.betterjavacode.springbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterjavacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterjavacodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betterjavacode/springbatchdemo/BetterjavacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterjavacode.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterjavacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
